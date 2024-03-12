e6702800cd0cb75f46373538ecdc84b5524998ec
Šimon Prokopec <prokopec.simon@nvsp.cz>
2024-03-12 14:19:07 +0100

    Split the core into methods, minor refactoring

--- GitChronicle.py
+++ GitChronicle.py
@@ -3,119 +3,115 @@ import subprocess
 from datetime import datetime
 import questionary
 
-os.system('cls')
+def get_default_path():
+    default_path = os.getcwd()
+    path_components = default_path.split("\\")
+    if not path_components[0].endswith("\\"):
+        path_components[0] += "\\"
+    return os.path.join(*path_components[:-1]) + "\\"
 
-# Set the default path to the current working directory
-default_path = os.getcwd()
-path_components = default_path.split("\\")
+def get_valid_path():
+    path = questionary.text("Enter the path to search for git repositories:", default=get_default_path()).ask()
+    while not os.path.exists(path):
+        print("Invalid path. Please enter a valid path.")
+        path = questionary.text("Enter the path to search for git repositories:", default=get_default_path()).ask()
+    return path
 
-# For some reason the joined path was missing \ after C:
-if not path_components[0].endswith("\\"):
-    path_components[0] += "\\"
+def get_repositories_with_commits(path, cmd):
+    repositories_with_commits = []
+    distinct_commit_authors = []
 
-# Remove the last element (file name) and rejoin the path components
-default_path = os.path.join(*path_components[:-1]) + "\\"
-# Ask user for the path to search for git repositories with the default value
-path = questionary.text("Enter the path to search for git repositories:", default=default_path).ask()
+    for root, dirs, files in os.walk(path):
+        for dir in dirs:
+            if dir.endswith('.git'):
+                repo_name = os.path.basename(root)
+                try:
+                    output = subprocess.check_output(cmd, cwd=root, shell=True).decode()
+                    if len(output.strip()) > 0:
+                        authors = set()
+                        for line in output.splitlines():
+                            line = line.strip()
+                            if line:
+                                parts = line.split(':')
+                                name = parts[1].split(' - ')[0].strip()
+                                authors.add(name)
+                                if name not in distinct_commit_authors:
+                                    distinct_commit_authors.append(name)
+                        repositories_with_commits.append({'name': repo_name, 'commits_from': authors})
+                except subprocess.CalledProcessError as e:
+                    print(f"Error executing git command in {repo_name}: {e}")
+                    continue
 
-# Validate if the path exists
-while not os.path.exists(path):
-    print("Invalid path. Please enter a valid path.")
-    path = questionary.text("Enter the path to search for git repositories:", default=default_path).ask()
+    return repositories_with_commits, distinct_commit_authors
 
-# Define the git command to browse for data in the last month and five days
-cmd = 'git log --since="1 month 5 days ago" --pretty=format:"%ad: %an - %s" --date=format:"%Y-%m-%d" --all'
+def get_users_to_show(distinct_commit_authors):
+    return questionary.checkbox("Select users to show commits for", choices=distinct_commit_authors).ask()
 
-# Initialize the dictionaries to hold the commit data and user data
-commit_data = {}
-repositories_with_commits = []
-distinct_commit_authors = []
+def get_ignore_repositories(repositories_with_commits, users_to_show):
+    return questionary.checkbox(
+        "Select repositories to ignore",
+        choices=[repo['name'] for repo in repositories_with_commits if any(user in users_to_show for user in repo['commits_from'])]
+    ).ask()
 
-os.system('cls')
-print('Loading repositories...')
+def get_repositories_without_commits(repositories_with_commits, users_to_show):
+    return [repo['name'] for repo in repositories_with_commits if not any(user in users_to_show for user in repo['commits_from'])]
 
-# Get the list of repositories with commits
-for root, dirs, files in os.walk(path):
-    for dir in dirs:
-        # Check if the directory is a git repository
-        if dir.endswith('.git'):
-            # Get the repository name
-            repo_name = os.path.basename(root)
-            # Execute the git command and decode the output
-            try:
-                output = subprocess.check_output(
-                    cmd, cwd=root, shell=True).decode()
-                if len(output.strip()) > 0:
-                    # Extract and add authors (users) to the set
-                    authors = set()
-                    for line in output.splitlines():
-                        line = line.strip()
-                        if line:
-                            parts = line.split(':')
-                            name = parts[1].split(' - ')[0].strip()
-                            authors.add(name)
-                            if name not in distinct_commit_authors:
-                                distinct_commit_authors.append(name)
-                    repositories_with_commits.append({'name': repo_name, 'commits_from': authors})
-            except subprocess.CalledProcessError as e:
-                print(f"Error executing git command in {repo_name}: {e}")
-                continue
+def load_commits(path, ignore_repositories, cmd):
+    commit_data = {}
 
-os.system('cls')
-# Ask user which users to show components for
-users_to_show = questionary.checkbox(
-    "Select users to show commits for",
-    choices=distinct_commit_authors
-).ask()
+    for root, dirs, files in os.walk(path):
+        for dir in dirs:
+            if dir.endswith('.git'):
+                repo_name = os.path.basename(root)
+                if repo_name in ignore_repositories:
+                    continue
+                output = subprocess.check_output(cmd, cwd=root, shell=True).decode()
+                output_lines = output.split('\n')
+                for line in output_lines:
+                    line = line.strip()
+                    if line != '':
+                        date_str, message = line.split(': ', 1)
+                        date = datetime.strptime(date_str, '%Y-%m-%d')
+                        if date not in commit_data:
+                            commit_data[date] = []
+                        commit_data[date].append((repo_name, message))
 
-# Ask user which repositories to ignore
-ignore_repositories = questionary.checkbox(
-    "Select repositories to ignore",
-    choices=[repo['name'] for repo in repositories_with_commits if any(user in users_to_show for user in repo['commits_from'])]
-).ask()
+    return commit_data
 
-#ignoring repositories that have no commits from selected users
-repositories_without_any_commits_from_selected_users = [repo['name'] for repo in repositories_with_commits if not any(user in users_to_show for user in repo['commits_from'])]
-ignore_repositories.extend(repositories_without_any_commits_from_selected_users)
+def print_commit_data(commit_data, users_to_show):
+    os.system('cls')
+    for date in sorted(commit_data.keys()):
+        commits = commit_data[date]
+        print(f'[{date.strftime("%Y-%m-%d")}]:')
+        for commit in commits:
+            if(len(users_to_show)==1):
+                print(f'    {commit[0]} - {commit[1].split(" - ")[1]}')
+            else:
+                print(f'    {commit[0]} - {commit[1]}')
 
-os.system('cls')
-print('Loading individual commits...')
-# Loop through each repository
-for root, dirs, files in os.walk(path):
-    for dir in dirs:
-        # Check if the directory is a git repository
-        if dir.endswith('.git'):
-            # Get the repository name
-            repo_name = os.path.basename(root)
-            if repo_name in ignore_repositories:
-                continue
-            # Execute the git command and decode the output
-            output = subprocess.check_output(
-                cmd, cwd=root, shell=True).decode()
-            # Split the output by newline characters
-            output_lines = output.split('\n')
-            # Loop through each line of output and extract the commit data
-            for line in output_lines:
-                line = line.strip()
-                if line != '':
-                    # Extract the date and commit message
-                    date_str, message = line.split(': ', 1)
-                    date = datetime.strptime(date_str, '%Y-%m-%d')
-                    # Add the commit data to the dictionary
-                    if date not in commit_data:
-                        commit_data[date] = []
-                    commit_data[date].append((repo_name, message))
+def main():
+    os.system('cls')
+    path = get_valid_path()
 
-os.system('cls')
+    cmd = 'git log --since="1 month 5 days ago" --pretty=format:"%ad: %an - %s" --date=format:"%Y-%m-%d" --all'
 
-# Print the commit data in order by date
-for date in sorted(commit_data.keys()):
-    commits = commit_data[date]
-    print(f'[{date.strftime("%Y-%m-%d")}]:')
-    for commit in commits:
-        if(len(users_to_show)==1):
-            # If there is only one author do not print the name in the output
-            print(f'    {commit[0]} - {commit[1].split(" - ")[1]}')
-        else:
-            # Include author if more than one author is selected
-            print(f'    {commit[0]} - {commit[1]}')
+
+    os.system('cls')
+    print('Loading repositories...')
+    repositories_with_commits, distinct_commit_authors = get_repositories_with_commits(path, cmd)
+
+    users_to_show = get_users_to_show(distinct_commit_authors)
+    ignore_repositories = get_ignore_repositories(repositories_with_commits, users_to_show)
+
+    repositories_without_commits = get_repositories_without_commits(repositories_with_commits, users_to_show)
+    ignore_repositories.extend(repositories_without_commits)
+
+    os.system('cls')
+    print('Loading individual commits...')
+
+    commit_data = load_commits(path, ignore_repositories, cmd)
+
+    print_commit_data(commit_data, users_to_show)
+
+if __name__ == "__main__":
+    main()
